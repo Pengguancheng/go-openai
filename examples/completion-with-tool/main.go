d5f78cac24d69822eba39e98f0e196dfb0b231db
eric.p <eric.p>
2024-06-16 18:17:47 +0800

    chore: fix ref use

--- examples/completion-with-tool/main.go
+++ examples/completion-with-tool/main.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/sashabaranov/go-openai"
-	"github.com/sashabaranov/go-openai/jsonschema"
+	"github.com/Pengguancheng/go-openai"
+	"github.com/Pengguancheng/go-openai/jsonschema"
 )
 
 func main() {
--- internal/test/helpers.go
+++ internal/test/helpers.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-	"github.com/sashabaranov/go-openai/internal/test/checks"
+	"github.com/Pengguancheng/go-openai/internal/test/checks"
 
 	"net/http"
 	"os"
